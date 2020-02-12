19001b5ac6cdf11061a6c50f0d4de7947889a358
Summer Chen <mp4bj6ruby@gmail.com>
2020-02-12 12:16:36 +0800

    axios撈值測試ok

--- store/tv/actions.js
+++ store/tv/actions.js
@@ -1,12 +1,10 @@
-import fetch from '@/config/fetch'
-
 export default {
   async getList ({ commit, dispatch }, payload) {
-    const { data } = await fetch.get('users', { params: { time: '999' } })
-    commit('gotList', data)
+    const list = await this.$axios.$get('users', { params: { time: payload.time } })
+    commit('gotList', list)
   },
   async getItem ({ commit, dispatch }, id) {
-    const { data } = await fetch.get(`users/${id}`, { params: { time: '999' } })
-    commit('gotItem', data)
+    const item = await this.$axios.$get(`users/${id}`)
+    commit('gotItem', item)
   }
 }

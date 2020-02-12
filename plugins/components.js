45aa9e6bf2864783b9124c0313629befccaa4c38
Summer Chen <mp4bj6ruby@gmail.com>
2020-02-12 12:42:11 +0800

    Global components設置

--- plugins/components.js
+++ plugins/components.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Footer from '../components/Footer'
 import ScrollTop from '../components/ScrollTop'
 import IndexScrollTop from '../components/IndexScrollTop'
+import Dashboard from '../components/Dashboard'
+
+Vue.component('Dashboard', Dashboard)
 Vue.component('Footer', Footer)
 Vue.component('ScrollTop', ScrollTop)
 Vue.component('IndexScrollTop', IndexScrollTop)

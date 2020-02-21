358ad1a1e53ca410909cf4124e1ca1eca1a0f8a9
Summer Chen <mp4bj6ruby@gmail.com>
2020-02-21 16:18:25 +0800

    整理api server

--- server/api.js
+++ server/api.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-// import session from 'express-session'
+
 // Create express router
 const router = express.Router()
 
@@ -9,12 +9,6 @@ const router = express.Router()
 const app = express()
 
 router.use(bodyParser())
-// router.use(session({
-//   secret: 'summer-is-cool',
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: { maxAge: 60000 }
-// }))
 router.use((req, res, next) => {
   Object.setPrototypeOf(req, app.request)
   Object.setPrototypeOf(res, app.response)
@@ -24,14 +18,8 @@ router.use((req, res, next) => {
 })
 
 router.post('/auth', (req, res) => {
-  console.log(req.body)
   req.session.auth = req.body.token
   return res.json({ ok: true, data: req.session.auth })
-  // if (req.body.username === 'demo' && req.body.password === 'demo') {
-  //   req.session.authUser = { username: 'demo' }
-  //   return res.json({ username: 'demo' })
-  // }
-  // res.status(401).json({ message: 'Bad credentials' })
 })
 
 router.delete('/auth', (req, res) => {

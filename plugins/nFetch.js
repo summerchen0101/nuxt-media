d014d1d88763a6c03c2f3df773cfabba6c19f55e
Summer Chen <mp4bj6ruby@gmail.com>
2020-02-24 13:58:01 +0800

    修改node api 的對應host

--- plugins/nFetch.js
+++ plugins/nFetch.js
@@ -1,5 +1,7 @@
-export default ({ app, store, $axios, redirect }, inject) => {
-  const baseURL = `http://${process.env.HOST}:${process.env.PORT}`
+export default ({ app, store, $axios, redirect, isDev }, inject) => {
+  const baseURL = isDev
+    ? `http://${process.env.HOST}:${process.env.PORT}`
+    : `http://${process.env.API_BASE_URL}`
   const nodeInstance = $axios.create({
     baseURL
   })

925b6849706bc6e43b2e8eab6153b393c2648784
Summer Chen <mp4bj6ruby@gmail.com>
2020-02-26 12:00:30 +0800

    修改api模組配置

--- plugins/apiHub.js
+++ plugins/apiHub.js
@@ -1,15 +1,29 @@
 import { AD } from '~/lib/api/ad'
 import { USER } from '~/lib/api/user'
+import { SITE } from '~/lib/api/site'
 
 const apiModules = {
-  ...AD,
-  ...USER
+  AD,
+  USER,
+  SITE
 }
-
 export default ({ app }, inject) => {
-  const api = Object.keys(apiModules).reduce((obj, key) => {
-    obj[key] = data => app.$fetch(apiModules[key], data)
-    return obj
-  }, {})
-  inject('api', api)
+  function makeApis (apiObj) {
+    return Object.keys(apiObj).reduce((obj, key) => {
+      obj[key] = data => app.$fetch(apiObj[key], data)
+      return obj
+    }, {})
+  }
+
+  const apis = Object.keys(apiModules)
+    .reduce((obj, moduleName) => {
+      const _module = apiModules[moduleName]
+      if (_module.namespaced) {
+        obj[moduleName] = makeApis(_module.apis)
+      } else {
+        obj = Object.assign({}, obj, makeApis(_module.apis))
+      }
+      return obj
+    }, {})
+  inject('api', apis)
 }
